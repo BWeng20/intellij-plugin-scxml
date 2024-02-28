924d88e98769cc2ed8d8b26abdaec1088b8eca21
Bernd <Bernd.Wengenroth@gmx.de>
2024-02-28 18:22:52 +0100

    Finished basic edit actions.

--- src/main/java/com/bw/graph/editor/DefaultDialogHandler.java
+++ src/main/java/com/bw/graph/editor/DefaultDialogHandler.java
@@ -1,5 +1,6 @@
 package com.bw.graph.editor;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -7,6 +8,8 @@ import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Default Dialog Handler, using a JDialog.
@@ -21,8 +24,8 @@ public class DefaultDialogHandler implements DialogHandler
 
 	}
 
-
 	private JDialog _dialog;
+	private boolean _handled = false;
 
 	@Override
 	public void openEditor(GraphPane graph, JComponent editor, Runnable endEdit, Runnable cancelEdit)
@@ -34,7 +37,27 @@ public class DefaultDialogHandler implements DialogHandler
 
 		_dialog.setLayout(new BorderLayout());
 
+		JButton ok = new JButton("Ok");
+		JButton cancel = new JButton("Cancel");
+
+		_handled = false;
+
 		JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		buttons.add(ok);
+		buttons.add(cancel);
+
+		ok.addActionListener(e -> {
+			_handled = true;
+			System.err.println("OK selected");
+			endEdit.run();
+		});
+
+		cancel.addActionListener(e -> {
+			_handled = true;
+			System.err.println("CANCEL selected");
+			cancelEdit.run();
+		});
+
 		_dialog.add(buttons, BorderLayout.SOUTH);
 		_dialog.setModal(false);
 		_dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -43,6 +66,20 @@ public class DefaultDialogHandler implements DialogHandler
 		_dialog.pack();
 		_dialog.setLocationRelativeTo(graph);
 		_dialog.setVisible(true);
+
+		_dialog.addWindowListener(new WindowAdapter()
+		{
+			@Override
+			public void windowDeactivated(WindowEvent e)
+			{
+				if (!_handled)
+				{
+					System.err.println("Editor Deactivated");
+					cancelEdit.run();
+					_handled = true;
+				}
+			}
+		});
 	}
 
 	@Override
